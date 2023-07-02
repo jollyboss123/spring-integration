b68794bc931b9dbd5f4546b0a1efb285afd69775
jolly6 <98voonfusing@gmail.com>
2023-07-02 17:29:48 +0800

    Looking at message source and polls

--- src/main/java/com/jolly/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/jolly/springintegration/SpringIntegrationApplication.java
@@ -9,10 +9,14 @@ import org.springframework.integration.core.GenericTransformer;
 import org.springframework.integration.core.MessageSource;
 import org.springframework.integration.dsl.IntegrationFlow;
 import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.stereotype.Component;
 
+import java.time.Duration;
 import java.time.Instant;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class SpringIntegrationApplication {
@@ -26,27 +30,35 @@ public class SpringIntegrationApplication {
         return MessageChannels.direct().getObject();
     }
 
-    private String text() {
+    private static String text() {
         return Math.random() > .5 ?
                 "Hello " + Instant.now() + " !" :
                 "Hola " + Instant.now() + " !";
     }
 
-    @Bean
-    IntegrationFlow flow() {
-        return IntegrationFlow
-                .from((MessageSource<String>) () ->
-                        MessageBuilder.withPayload(text()).build(),
-                        poller -> poller.poller(pm -> pm.fixedRate(100)))
-                .filter(String.class, source -> source.contains("Hola"))
-                .channel(greetings())
-                .get();
+    /**
+     * message source are like suppliers
+     * that have to invoked i.e. polled/inquired
+     * have to provide poller definition
+     * by trigger, cron, fixed rate or delay
+     */
+    @Component
+    static class CustomMessageSource implements MessageSource<String> {
+
+        @Override
+        public Message<String> receive() {
+            return MessageBuilder.withPayload(text()).build();
+        }
     }
 
     @Bean
-    IntegrationFlow flow1() {
+    IntegrationFlow flow(CustomMessageSource customMessageSource) {
         return IntegrationFlow
-                .from(greetings())
+                .from(customMessageSource,
+                        sourcePollingChannelAdapterSpec -> sourcePollingChannelAdapterSpec.poller(
+                                pollerFactory -> pollerFactory.fixedRate(Duration.ofSeconds(3))
+                        ))
+                .filter(String.class, source -> source.contains("Hola"))
                 .transform((GenericTransformer<String, String>) String::toUpperCase)
                 .handle((GenericHandler<String>) (payload, headers) -> {
                     System.out.println("the payload is " + payload);
