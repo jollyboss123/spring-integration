f41487c5113cafdeb89e0506b8b1a01f88c53563
jolly6 <98voonfusing@gmail.com>
2023-07-04 09:10:27 +0800

    message gateway interface

--- src/main/java/com/jolly/springintegration/SpringIntegrationApplication.java
+++ src/main/java/com/jolly/springintegration/SpringIntegrationApplication.java
@@ -36,12 +36,15 @@ public class SpringIntegrationApplication {
         Thread.currentThread().join();
     }
 
-    @Bean
+    static final String REQUESTS_CHANNEL = "requests";
+    static final String REPLIES_CHANNEL = "replies";
+
+    @Bean(name = REQUESTS_CHANNEL)
     MessageChannel greetingsRequest() {
         return MessageChannels.direct().getObject();
     }
 
-    @Bean
+    @Bean(name = REPLIES_CHANNEL)
     DirectChannel greetingsReply() {
         return MessageChannels.direct().getObject();
     }
@@ -71,52 +74,50 @@ public class SpringIntegrationApplication {
     IntegrationFlow flow() {
         return IntegrationFlow
                 .from(greetingsRequest())
-                .filter(String.class, source -> source.contains("Hola"))
+//                .filter(String.class, source -> source.contains("Hola"))
                 .transform((GenericTransformer<String, String>) String::toUpperCase)
                 .channel(greetingsReply())
                 .get();
     }
 
-    @Bean
-    IntegrationFlow inboundFileSystemFlow() {
-        File directory = new File(SystemPropertyUtils.resolvePlaceholders("${HOME}/Desktop/in"));
-        FileInboundChannelAdapterSpec files = Files.inboundAdapter(directory)
-                .autoCreateDirectory(true);
-        return IntegrationFlow
-                .from(files, poller -> poller.poller(pm -> PollerFactory.fixedRate(Duration.ofSeconds(1))))
-                .transform(new FileToStringTransformer()) // because greetingsRequest expects a String to filter
-                .handle((GenericHandler<String>) (payload, headers) -> {
-                    System.out.println("----- start of the line -----");
-                    headers.forEach((key, value) -> System.out.println(key + " = " + value));
-                    return payload;
-                })
-                .channel(greetingsRequest())
-                .get();
-    }
-
-    @Bean
-    IntegrationFlow outboundFileSystemFlow() {
-        File directory = new File(SystemPropertyUtils.resolvePlaceholders("${HOME}/Desktop/out"));
-        return IntegrationFlow
-                .from(greetingsReply())
-                .handle((GenericHandler<String>) (payload, headers) -> {
-                    System.out.println("----- end of the line -----");
-                    headers.forEach((key, value) -> System.out.println(key + " = " + value));
-                    return payload;
-                })
-                .handle(Files.outboundAdapter(directory).autoCreateDirectory(true))
-                .get();
-    }
+//    @Bean
+//    IntegrationFlow inboundFileSystemFlow() {
+//        File directory = new File(SystemPropertyUtils.resolvePlaceholders("${HOME}/Desktop/in"));
+//        FileInboundChannelAdapterSpec files = Files.inboundAdapter(directory)
+//                .autoCreateDirectory(true);
+//        return IntegrationFlow
+//                .from(files, poller -> poller.poller(pm -> PollerFactory.fixedRate(Duration.ofSeconds(1))))
+//                .transform(new FileToStringTransformer()) // because greetingsRequest expects a String to filter
+//                .handle((GenericHandler<String>) (payload, headers) -> {
+//                    System.out.println("----- start of the line -----");
+//                    headers.forEach((key, value) -> System.out.println(key + " = " + value));
+//                    return payload;
+//                })
+//                .channel(greetingsRequest())
+//                .get();
+//    }
+//
+//    @Bean
+//    IntegrationFlow outboundFileSystemFlow() {
+//        File directory = new File(SystemPropertyUtils.resolvePlaceholders("${HOME}/Desktop/out"));
+//        return IntegrationFlow
+//                .from(greetingsReply())
+//                .handle((GenericHandler<String>) (payload, headers) -> {
+//                    System.out.println("----- end of the line -----");
+//                    headers.forEach((key, value) -> System.out.println(key + " = " + value));
+//                    return payload;
+//                })
+//                .handle(Files.outboundAdapter(directory).autoCreateDirectory(true))
+//                .get();
+//    }
 }
 
 @Component
 class Runner implements ApplicationRunner {
     private final GreetingsClient greetingsClient;
-    private final DirectChannel greetingsReply;
 
-    Runner(GreetingsClient greetingsClient, DirectChannel greetingsReply) {
+    Runner(GreetingsClient greetingsClient) {
         this.greetingsClient = greetingsClient;
-        this.greetingsReply = greetingsReply;
     }
 
     @Override
@@ -126,12 +127,22 @@ class Runner implements ApplicationRunner {
 //        for (int i = 0; i < 100; i++) {
 //            this.greetingsClient.greet(SpringIntegrationApplication.text());
 //        }
+        String reply = this.greetingsClient.greet("Bussing");
+        System.out.println("the reply: " + reply);
     }
 }
+
+/**
+ * the problem with gateway is that it waits for a reply
+ * when the message does not pass or fails to (in this case does not contain filter text "Hola"),
+ * there is nothing that would produce a reply
+ * thus, it's stuck there and is infinite by default
+ */
 
 @MessagingGateway
 interface GreetingsClient {
 
-    @Gateway(requestChannel = "greetingsRequest")
-    void greet(String text);
+    @Gateway(requestChannel = SpringIntegrationApplication.REQUESTS_CHANNEL,
+    replyChannel = SpringIntegrationApplication.REPLIES_CHANNEL)
+    String greet(String text);
 }
