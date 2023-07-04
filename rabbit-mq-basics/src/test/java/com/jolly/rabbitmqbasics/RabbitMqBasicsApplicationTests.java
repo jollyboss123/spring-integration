27cf899815dd61c6731134cc4535e1570dd2270e
jolly6 <98voonfusing@gmail.com>
2023-07-04 21:17:38 +0800

    integration flow with rabbitmq

---
+++ rabbit-mq-basics/src/test/java/com/jolly/rabbitmqbasics/RabbitMqBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolly.rabbitmqbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
